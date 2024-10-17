42b9c9cf76151a662bec0e8290389311a192a307
aashnajoshi <aashnahetal@gmail.com>
2024-10-17 15:22:26 +0530

    .

--- app/views.py
+++ app/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here. (request-handler: request -> reponse)
-
 def health_check(request):
     return HttpResponse("Congrats! Django is working")
 
@@ -12,11 +11,9 @@ def greet(request):
 def user_data(request):
     dummy_data = {
         'users': [
-            {'name': 'Alice', 'age':12},
-            {'name': 'Bob', 'age': 23},
-            {'name': 'Charlie', 'age': 34},
-            {'name': 'Diana', 'age': 43},
-            {'name': 'Eve', 'age': 21}
-        ]
-    }
+            {'name': 'Qwerty', 'age':12},
+            {'name': 'Asdfg', 'age': 23},
+            {'name': 'Zxcvb', 'age': 34},
+            {'name': 'Poiuy', 'age': 43},
+            {'name': 'Lkjhg', 'age': 21}]}
     return render(request, 'welcome.html', {'info': dummy_data})
