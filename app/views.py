06615d0976241b5cfdcf7a477090d71e9ce94ce7
aashnajoshi <aashnahetal@gmail.com>
2024-10-18 14:39:53 +0530

    .

--- app/views.py
+++ app/views.py
@@ -4,12 +4,12 @@ from .models import form
 
 # Create your views here. (request-handler: request -> reponse)
 def health_check(request):
-    return HttpResponse("Congrats! Django is working")
+    return HttpResponse("<center>Congrats! Django is working</center>") # Usage of HTTP response for Static
 
 def greet(request):
-    return render(request, 'index.html', {'name': 'Qwerty'})
+    return render(request, 'index.html', context = {'name': 'Qwerty'}) # Usage of render function for Static
 
-def user_data(request):
+def user_data(request): # Usage of render function for Partially Dynamic (data from views.py file)
     dummy_data = {
         'users': [
             {'name': 'Qwerty', 'age':12},
@@ -17,8 +17,8 @@ def user_data(request):
             {'name': 'Zxcvb', 'age': 34},
             {'name': 'Poiuy', 'age': 43},
             {'name': 'Lkjhg', 'age': 21}]}
-    return render(request, 'index.html', {'info': dummy_data})
+    return render(request, 'index.html', context = {'info': dummy_data})
 
-def form_data(request):
+def form_data(request): # Usage of render function for Dynamic (data from database)
     data = form.objects.all()
-    return render(request, 'index.html', {'data': data})
+    return render(request, 'index.html', context = {'data': data})
