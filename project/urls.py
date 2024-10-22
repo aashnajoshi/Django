1b5b1ad2ea18676c5556e4c8f8f7ea32fcb50c5b
aashnajoshi <aashnahetal@gmail.com>
2024-10-22 15:22:19 +0530

    .

--- project/urls.py
+++ project/urls.py
@@ -27,7 +27,4 @@ urlpatterns = [
     path('debug/', include(debug_toolbar.urls)),
     path('app/', include('app.urls')),
     path('stream/', include('stream.urls')),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
