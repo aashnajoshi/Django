3f91b90ae04255a5a0a9138a547d41862104e2a2
Aashna Joshi <aashnahetal@gmail.com>
2024-11-28 15:32:24 +0530

    .

--- project/settings.py
+++ project/settings.py
@@ -137,7 +137,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'aashnahetal@gmail.com'
-EMAIL_HOST_PASSWORD = 'ouis gelm dyrl naf'
+EMAIL_HOST_PASSWORD = 'weqcljsnjuefhick'
 
 # TinyMCE settings:
 TINYMCE_DEFAULT_CONFIG = {
