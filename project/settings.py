aa038386701acda4a999e9fa77b940614dd41906
aashnajoshi <aashnahetal@gmail.com>
2025-01-31 16:35:00 +0530

    .

--- project/settings.py
+++ project/settings.py
@@ -107,13 +107,16 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
+from dotenv import load_dotenv
+
+load_dotenv()
 # For sending Mail:
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'aashnahetal@gmail.com'
-EMAIL_HOST_PASSWORD = 'weqcljsnjuefhick'
+EMAIL_HOST_USER = os.getenv('mail')
+EMAIL_HOST_PASSWORD = os.getenv('password')
 
 # TinyMCE settings:
 TINYMCE_DEFAULT_CONFIG = {
